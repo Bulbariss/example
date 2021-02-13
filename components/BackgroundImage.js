e78b7345fb5b2197f163adb55a9cf3eaa95575a0
Vladimir Radionovsky <dudefromyalta@gmail.com>
2021-02-13 15:40:00 +0300

    better bg image

--- components/BackgroundImage.js
+++ components/BackgroundImage.js
@@ -15,24 +15,23 @@ export default function BackgroundImage({
     )[0];
     imagePlaceholder.classList.add("loaded");
   }
-
   return (
     <>
       <div ref={imageRef} className={`relative w-screen ${className}`}>
         <div className="absolute top-0 left-0 w-full h-full">
-          <div
-            style={{
-              backgroundImage: `url("/_next/image?url=/${image}&w=40&q=75")`,
-            }}
+          <Image
+            src={`/_next/image?url=/${image}&w=40&q=75`}
             className="z-10 w-full h-full delay-200 bg-image"
-          ></div>
+            layout="fill"
+            objectFit="cover"
+            priority="true"
+          />
         </div>
         <Image
           onLoad={() => imageLoaded()}
           layout="fill"
           quality={quality || 75}
           objectFit="cover"
-          style={{ visibility: "visible" }}
           src={"/" + image}
           alt={alt ? alt : "test"}
           className="w-full h-full onload"
